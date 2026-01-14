86a1a693386c77dd463e5a0390a8dbb4a24cb1c5
ErickgSoldier <Erickgabriel500@gmail.com>
2026-01-14 09:03:40 -0300

    First commit

---
+++ src/main/java/com/example/literalura/demo/LiterALuraApplication.java
@@ -0,0 +1,13 @@
+package com.example.literalura.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterALuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterALuraApplication.class, args);
+	}
+
+}
